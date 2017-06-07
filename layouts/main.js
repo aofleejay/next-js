737fdd99087c4fd104321a75797223b920f24ae1
aofleejay <aofleejay@hotmail.com>
2017-06-08 00:17:51 +0700

    add bulma

--- layouts/main.js
+++ layouts/main.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
+import bulma from 'bulma/css/bulma.css'
 
 export default ({ children }) => (
   <div>
@@ -8,6 +9,7 @@ export default ({ children }) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Navbar />
+    <style dangerouslySetInnerHTML={{ __html: bulma }} />
     { children }
   </div>
 )
